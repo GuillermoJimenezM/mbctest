172cbbd6493061e1e974c989171f51bf23a2d33d
Guillermo Jimenez <guillermo.jimenez@orangeloops.com>
2023-11-02 22:59:11 -0400

    Init commit

---
+++ nimbleTest/nimbleTest/nimbleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  nimbleTestApp.swift
+//  nimbleTest
+//
+//  Created by Guillermo Jimenez on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct nimbleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
